cfe7c1e8996d9b70e9185fce2bc968c6eddfc75d
renzovalverde <ravf.escorpio@gmail.com>
2021-10-18 16:55:48 -0500

    Practica 06 - listar modadalidadPorJson

---
+++ src/main/java/com/empresa/entity/FiltroModalidad.java
@@ -0,0 +1,27 @@
+package com.empresa.entity;
+
+public class FiltroModalidad {
+	
+	private String nombre;
+	private String sede;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getSede() {
+		return sede;
+	}
+	public void setSede(String sede) {
+		this.sede = sede;
+	}
+	
+	
+	
+
+}
--- src/main/java/com/empresa/repository/ModalidadRepository.java
+++ src/main/java/com/empresa/repository/ModalidadRepository.java
@@ -1,10 +1,28 @@
 package com.empresa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 
 public interface ModalidadRepository extends JpaRepository<Modalidad, Integer>  {
 
 	
+	@Query("select d from Modalidad d where "
+			+ "( :m_sede is '' or d.sede = :m_sede ) and "
+			+ "( :m_nom is '' or d.nombre like :m_nom )")
+public abstract List<Modalidad> listaDocentePorSedeNombre(
+							 	@Param("m_sede") String sede, 
+							 	@Param("m_nom") String nombre);
+	
+	
+	@Query("select d from Modalidad d where "
+			+ "( :#{#fil.sede} is '' or d.sede = :#{#fil.sede} ) and "
+			+ "( :#{#fil.nombre} is '' or d.nombre like :#{#fil.nombre} )  ")
+	public abstract List<Modalidad> listaPorFiltro(@Param("fil")FiltroModalidad filtro);
 }
